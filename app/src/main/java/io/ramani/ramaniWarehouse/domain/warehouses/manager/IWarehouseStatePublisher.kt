1ed8d902b24c1de0271bfa6f372e822e95c82011
Amr <amr@ramani.io>
2022-02-02 14:20:27 +0200

    remove unused data

--- app/src/main/java/io/ramani/ramaniWarehouse/domain/warehouses/manager/IWarehouseStatePublisher.kt
+++
@@ -1,9 +0,0 @@
-package io.ramani.ramaniWarehouse.domain.warehouses.manager
-
-import io.ramani.ramaniWarehouse.app.warehouses.mainNav.model.WarehouseModelView
-import io.ramani.ramaniWarehouse.domain.warehouses.models.WarehouseModel
-
-interface IWarehouseStatePublisher {
-    fun notifyWarehouseListLoaded(warehouseList: MutableList<WarehouseModelView>)
-    fun notifyCurrentWarehouseChanged(currentWarehouse: WarehouseModel?)
-}
