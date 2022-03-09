e020de37ef3b3cdd671f9bc50f4a62709d97452d
ComradeHadi <comradehadi@ramani.io>
2022-03-09 09:25:48 +0000

    changed scaled height

--- app/src/main/java/io/ramani/ramaniWarehouse/domainCore/printer/MobiIoTDevice.kt
+++ app/src/main/java/io/ramani/ramaniWarehouse/domainCore/printer/MobiIoTDevice.kt
@@ -73,7 +73,7 @@ class MobiIoTDevice(val context: Context) : POSDevice {
         try {
             //CsPrinter.printBitmap(bitmap,0)
            // val is_ticket = context.resources.openRawResource(bitmap)
-//              val newBitmap =  CsPrinter.getResizedBitmap(bitmap, 400, 500)
+              val newBitmap =  CsPrinter.getResizedBitmap(bitmap, 400, 500)
 //            CsPrinter.printSetDarkness(0)
 //
 //            CsPrinter.printBitmap(newBitmap, 0)
@@ -82,25 +82,26 @@ class MobiIoTDevice(val context: Context) : POSDevice {
 //
 //            Log.d("$TAG text error",errorMessage.toString())
 //
-//            var inputStreamToByte: ByteArray? = null
-//            try {
-//               // inputStreamToByte = InputStreamToByte(bitmap)
-//                //Convert bitmap to byte array
-//                inputStreamToByte = bitmap.toByteArray()
-//            } catch (e: IOException) {
-//                e.printStackTrace()
-//                CsPrinter.printBitmap(bitmap)
-//            }
+            var inputStreamToByte: ByteArray? = null
+            try {
+               // inputStreamToByte = InputStreamToByte(bitmap)
+                //Convert bitmap to byte array
+                inputStreamToByte = newBitmap.toByteArray()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                CsPrinter.printBitmap(bitmap)
+            }
             if (!Build.MODEL.contains("MPE")) {
 
                // val result = CsPrinter.printBitmap(inputStreamToByte, 0)
-                val newBitmap =  CsPrinter.getResizedBitmap(bitmap, 400, 500)
+               // val newBitmap =  CsPrinter.getResizedBitmap(bitmap, 400, 500)
                 CsPrinter.printSetDarkness(0)
 
-                CsPrinter.printBitmap(newBitmap, 0)
+                //CsPrinter.printBitmap(newBitmap, 0)
+                val result = CsPrinter.printBitmap(inputStreamToByte, 0)
                 //CsPrinter.printBitmap(inputStreamToByte, 0)
-               // Log.e("print result bitmap", result.toString() + "")
-                Toast.makeText(context, "1", Toast.LENGTH_LONG).show()
+                //Log.e("print result bitmap", result.toString() + "")
+                Toast.makeText(context, result.toString() + "", Toast.LENGTH_LONG).show()
             } else {
                // CsPrinter.printBitmapMPE(newBitmap.toByteArray(), 0)
                 Toast.makeText(context, "2", Toast.LENGTH_LONG).show()
